90b251fdd34951728412f6bfd08ff60561f66b89
Fernando Angeli <fernando_angeli@hotmail.com>
2022-05-09 18:12:34 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,3 +1,4 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import "./styles.css"
 
@@ -5,6 +6,25 @@ function Listing(){
     return(
         <>
             <Pagination/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard/>
+                    </div>
+                </div>
+            </div>
         </>
         )
 }
