3b8ad1f7cebad3fc92bf3fcdd13780d090169ba9
Fernando Angeli <fernando_angeli@hotmail.com>
2022-05-09 14:19:05 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,11 @@
+import Pagination from "components/Pagination";
+import "./styles.css"
+
 function Listing(){
     return(
-        <h1>Página de listagem</h1>
-    )
+        <>
+            <Pagination/>
+        </>
+        )
 }
 export default Listing;
