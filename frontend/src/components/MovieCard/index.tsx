434a92de85533f922b3261bd9a80e1450557250a
Fernando Angeli <fernando_angeli@hotmail.com>
2022-05-13 11:44:47 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -11,15 +11,15 @@ function MovieCard({ movie } : Props){
 
     return (
         <div>
-        <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-         <div className="dsmovie-card-bottom-container">
-        <h3>{movie.title}</h3>
-        <MovieScore />
-        <Link to ={`/form/{movie.id}`}>
-        <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-        </Link>
-    </div>
-</div>
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="dsmovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <MovieScore count={movie.count} score={movie.score} />
+                <Link to ={`/form/{movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                </Link>
+            </div>
+        </div>
     )
 }
 
