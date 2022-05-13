5c446085fac199ec7a914649475bdc2fe4c865b7
Fernando Angeli <fernando_angeli@hotmail.com>
2022-05-13 13:34:37 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -29,16 +29,19 @@ function Listing(){
         });
     },[pageNumber]);
     
+    const handlePageChange = (newPageNumber : number)  => {
+        setPageNumber(newPageNumber);
+    }
+
     return(
         <>
-            <Pagination/>
+            <Pagination page={page} onChange={handlePageChange}/>
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
-                        
-                            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                                <MovieCard movie={movie}/>
-                            </div>
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie}/>
+                        </div>
                     )
                     )}
                 </div>
